263fb114b54a017f4c2fe0fba95d4b2185c4a4aa
Zack Cuddy <zcuddy23@gmail.com>
2019-02-21 15:20:40 -0600

    Styles and top nav

---
+++ src/components/TopNav.vue
@@ -0,0 +1,35 @@
+<template>
+    <nav class="mx-2 flex align-center">
+        <g-link :to="{ name: 'home' }">
+            <g-image src="/images/logo_transparent.png" width="100px" />
+        </g-link>
+        <div class="links">
+            <g-link class="pl-1" :to="{ name: 'portfolio' }">Portfolio</g-link>
+            <g-link class="pl-1" :to="{ name: 'contact' }">Contact</g-link>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'TopNav'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/styles/colors.scss';
+    nav {
+        position: sticky;
+        top: 4rem;
+        height: 0;
+        
+        .links {
+            a {
+                text-transform: uppercase;
+                color: color(white);
+                text-decoration: none;
+                font-size: 1.5em;
+            }
+        }
+    }
+</style>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,46 +1,17 @@
 <template>
-  <div class="layout">
-    <!--
-    <header class="header">
-      <strong>
-        <g-link :to="{ name: 'home' }">{{ $static.metaData.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" :to="{ name: 'home' }">Home</g-link>
-        <g-link class="nav__link" :to="{ name: 'about' }">About</g-link>
-      </nav>
-    </header>
-    -->
+  <main class="layout">
+    <top-nav />
     <slot/>
-  </div>
+  </main>
 </template>
 
-<static-query>
-query {
-  metaData {
-    siteName
+<script>
+import TopNav from '~/components/TopNav'
+
+export default {
+  name: 'Default',
+  components: {
+    TopNav
   }
 }
-</static-query>
-
-<style>
-@import url('https://fonts.googleapis.com/css?family=Rubik');
-
-h1,h2,h3,h4,h5,h6 {
-  margin: 0;
-  padding: 0;
-}
-
-body {
-  height: 300vh;
-  font-family: 'Rubik', sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-  background-image: url('/images/Background.png');
-  background-position-x: 50%;
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-color: #212121;
-}
-</style>
+</script>
