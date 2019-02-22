4e68044d88002a54de5a3cbc12d8e81f7b301d23
Zack Cuddy <zcuddy23@gmail.com>
2019-02-22 15:51:07 -0600

    Hero component

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,31 +1,17 @@
 <template>
   <Layout>
-    <article class="flex">
-      <div class="content text-center m-a flex-1">
-        <h1>Zack Cuddy</h1>
-        <h3>Developer.</h3>
-      </div>
-      <div class="img-container flex-1" />
-    </article>
+    <hero />
   </Layout>
 </template>
 
-<style lang="scss" scoped>
-@import '../assets/styles/colors.scss';
+<script>
+import Hero from '~/components/Hero'
 
-  article {
-    min-height: 100vh;
-
-    .content {
-      font-size: 2em;
-      color: color(white);
-    }
-
-    .img-container {
-      background-position: 50%;
-      background-image: url('/images/Hero.jpg');
-      background-repeat: no-repeat;
-      background-size: cover;
-    }
+export default {
+  name: 'Home',
+  components: {
+    Hero
   }
-</style>
+}
+</script>
+
