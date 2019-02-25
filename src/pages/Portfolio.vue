0235b3ff42c5ce8a963667693eb7ad026ff144b3
Zack Cuddy <zcuddy23@gmail.com>
2019-02-25 00:13:21 -0600

    Porfolio Mocked

--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -1,7 +1,10 @@
 <template>
     <Layout>
-        <article class="flex">
-            <h1 class="m-a color-white" v-for="edge in $page.projects.edges" :key="edge.node.id">{{ edge.node.title }}</h1>
+        <article class="mx-4">
+          <a v-for="edge in $page.projects.edges" :key="edge.node.id" :href="edge.node.url" target="_blank">
+            <div class="img-container mb-1" :style="{ 'background-image': `url(${edge.node.image.url})` }" :aria-label="edge.node.title" />
+            <h2>{{ edge.node.title }}</h2>
+          </a>
         </article>
     </Layout> 
 </template>
@@ -30,7 +33,34 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../assets/styles/colors.scss';
+
   article {
-    min-height: 100vh;
+    margin-top: 10rem;
+    display: grid;
+    grid-gap: 1rem;
+    grid-template-columns: repeat(auto-fill, minmax(850px, 1fr));
+
+    a {
+      background-color: white;
+      color: color(blue);
+      text-align: center;
+      box-shadow: 0 0.25rem 0.5rem 0 rgba(color(greyLight), .06);
+      padding-bottom: 1rem;
+
+      &:hover {
+        box-shadow: 0 1rem 1.5rem 0 rgba(color(greyLight), .1), 0 0.25rem 0.5rem 0 rgba(color(greyLight), .06);
+        transform: scale(1.005);
+        color: color(blueDark);
+      }
+      
+      .img-container {
+        width: 100%;
+        height: 300px;
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: 50% 0;
+      }
+    }
   }
 </style>
