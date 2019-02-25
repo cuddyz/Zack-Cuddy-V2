e4191e8521e31dc6b2b004c4f6ee43f814efba0d
Zack Cuddy <zcuddy23@gmail.com>
2019-02-25 01:20:18 -0600

    Done?

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -19,16 +19,34 @@ export default {
 
 <style lang="scss" scoped>
 @import '../assets/styles/colors.scss';
+@import '../assets/styles/breaks.scss';
+
     nav {
         position: sticky;
         top: 4rem;
         height: 0;
         
         .logo {
-            width: 100px;
+            width: 70px;
             &:hover {
-                width: 110px;
+                width: 80px;
                 cursor: pointer;
+            }
+            
+            @media (min-width: breaks(phablet)) {
+                width: 80px;
+
+                 &:hover {
+                    width: 90px;
+                }
+            }
+
+            @media (min-width: breaks(laptop)) {
+                width: 100px;
+
+                 &:hover {
+                    width: 110px;
+                }
             }
         }
         
@@ -36,9 +54,25 @@ export default {
             a {
                 padding-left: 1rem;
                 text-transform: uppercase;
-                font-size: 1.5em;
+                font-size: 1.1em;
 
                 &:hover {
+                    font-size: 1.2em;
+                }
+            }
+
+            @media (min-width: breaks(laptop)) {
+                font-size: 1.3em;
+
+                 &:hover {
+                    font-size: 1.4em;
+                }
+            }
+
+            @media (min-width: breaks(desktop)) {
+                font-size: 1.5em;
+
+                 &:hover {
                     font-size: 1.6em;
                 }
             }
