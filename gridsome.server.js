c89234dceb9e82806de2e7aade6c92dc232d8378
Zack Cuddy <zcuddy23@gmail.com>
2019-02-24 23:26:16 -0600

    Graphql

--- gridsome.server.js
+++ gridsome.server.js
@@ -5,8 +5,57 @@
 // Changes here requires a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
+const axios = require('axios')
+
+const accessToken = '90542dc69f6d39e262ac6d0ae8a79ce4c8918e0a8104f3dbff9fdab1c613b432'
+const spaceId = 'yg0u7ppb2cxm'
+
 module.exports = function (api) {
-  api.loadSource(store => {
-    // Use the Data store API here: https://gridsome.org/docs/data-store-api
+  api.loadSource(async store => {
+
+    const promises = []
+    promises.push(axios.get(`https://cdn.contentful.com/spaces/${spaceId}/entries?access_token=${accessToken}`))
+    promises.push(axios.get(`https://cdn.contentful.com/spaces/${spaceId}/assets?access_token=${accessToken}`))
+
+    await Promise.all(promises).then(res => {
+      let projects = res[0].data
+      projects = projects.items
+
+      let assets = res[1].data
+      assets = assets.items
+
+      const projectType = store.addContentType({
+        typeName: 'Projects'
+      })
+
+      const assetType = store.addContentType({
+        typeName: 'ProjectAssets'
+      })
+
+
+      for (const item of projects) {
+        projectType.addNode({
+          id: item.sys.id,
+          title: item.fields.title,
+          fields: {
+            url: item.fields.url,
+            image: item.fields.image
+          }
+        })
+      }
+
+      for (const item of assets) {
+        assetType.addNode({
+          id: item.sys.id,
+          title: item.fields.title,
+          fields: {
+            url: item.fields.file.url,
+          }
+        })
+      }
+    })
+    .catch(e => {
+      console.log(e)
+    })
   })
 }
