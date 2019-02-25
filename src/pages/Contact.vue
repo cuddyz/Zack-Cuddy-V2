6670b793f4af58be20f097069468718f2dbfda8c
Zack Cuddy <zcuddy23@gmail.com>
2019-02-25 00:25:17 -0600

    Contact

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,19 +1,106 @@
 <template>
-    <Layout>
-        <article class="flex">
-            <h1 class="m-a color-white">Contact Me!</h1>
-        </article>
-    </Layout>
+<Layout>
+  <article class="grid">
+    <h1 class="pt-1 pb-1">Need help with a project?</h1>
+    <form id="contactForm" name="contactForm" method="POST" action="/" class="grid" netlify>
+      <input name="form-name" value="contactForm" type="hidden" />
+      <input name="name" :class="{'error': errors.fields && !contact.name }" v-model="contact.name" type="text" placeholder="Full Name" />
+      <input name="email" :class="{'error': (errors.fields && !contact.email) || errors.email }" v-model="contact.email" type="email" placeholder="Email" />
+      <textarea name="details" :class="{'error': errors.fields && !contact.details }" v-model="contact.details" placeholder="Details" />
+      <button class="hidden" type="submit">Send</button>
+    </form>
+    <button @click="submit" class="dark">Submit</button>
+    <h4 class="error" v-if="errors.fields">Make sure all the fields are filled out.</h4>
+    <h4 class="error" v-if="errors.email">That email doesn't look valid to me.</h4>
+    <h4 class="error" v-if="errors.post">Uh oh! Something went wrong with the POST. I console.log() it if you want to take a look!</h4>
+  </article>
+</Layout>
 </template>
 
 <script>
 export default {
-    name: 'Contact'
+  name: 'Contact',
+  data () {
+    return {
+      contact: {
+        name: '',
+        email: '',
+        details: ''
+      },
+      errors: {
+        fields: false,
+        email: false
+      }
+    }
+  },
+  methods: {
+    submit: function() {
+      if (!this.validForm()) {
+        return
+      }
+
+      document.getElementById('contactForm').submit()
+    },
+    validForm: function() {
+      if (!this.contact.name || !this.contact.email || !this.contact.details) {
+        this.errors.fields = true
+        return false
+      }
+
+      this.errors.fields = false
+
+      var emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      if (!emailRegex.test(this.contact.email)) {
+        this.errors.email = true
+        return false
+      }
+
+      this.errors.email = false
+      return true
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+  @import '../assets/styles/colors.scss';
+  @import '../assets/styles/breaks.scss';
+
   article {
-    min-height: 100vh;
+    height: 80vh;
+    display: grid;
+    grid-gap: 1rem;
+    justify-items: center;
+    align-content: center;
+
+    form {
+      text-align: center;
+      width: 50%;
+      grid-gap: 1rem;
+
+      @media (max-width: breaks(tablet)) {
+        width: 80%;
+      }
+    }
+
+    input,
+    textarea {
+      font-size: 1.2rem;
+      margin: 1rem;
+
+      &.error {
+        border-color: color(redDark);
+      }
+    }
+
+    h4 {
+      font-size: 1.1rem;
+      &.error {
+        color: color(redDark);
+      }
+    }
+  }
+  .hidden {
+    display: none;
   }
 </style>
